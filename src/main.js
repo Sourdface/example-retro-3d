80d26776a4b270695f30a6b992b806a8c27a2c51
Brad K. Harms <brad.k.harms@gmail.com>
2022-09-02 02:43:29 -0500

    Cleanup for main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 // @ts-check
 
+/**
+ * @module Main
+ *
+ * Entry point for the app containing the main update loop.
+ *
+ * Most work is offloaded to other modules. This file mainly serves to
+ */
+
 import * as Coin from './coin.js';
-import * as Input from './input.js';
-import * as Draw3D from './draw3d.js';
 import * as Draw2D from './draw2d.js';
+import * as Draw3D from './draw3d.js';
+import * as Editor from './editor.js';
+import * as Input from './input.js';
+import * as Level from './level.js';
 import * as Player from './player.js';
 import * as Wall from './wall.js';
-import * as Level from './level.js';
-import * as Editor from './editor.js';
 
+/**
+ * Application entry point.
+ */
 window.onload = () => {
   Draw2D.setup();
   Draw3D.setup();
@@ -22,6 +33,13 @@ window.onload = () => {
   update();
 }
 
+
+/**
+ * Main update loop body.
+ *
+ * This function runs once per frame. It is responsible for calling the
+ * update functions for all other modules that need it.
+ */
 function update() {
   Input.preUpdate();
 
